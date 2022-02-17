d8993eb8b7421ebafe609b498586492af15b8013
Jandro Salgado <salgadojandro@gmail.com>
2022-02-17 14:34:11 +0100

    [ADD] Nuevo proyecto renombrado - Limpio

--- TRABAJO/angular_Alejandro_Salgado/src/app/app.component.spec.ts
+++ TRABAJO/angular_Alejandro_Salgado/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'appProyecto'`, () => {
+  it(`should have as title 'angular_Alejandro_Salgado'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('appProyecto');
+    expect(app.title).toEqual('angular_Alejandro_Salgado');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('appProyecto app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular_Alejandro_Salgado app is running!');
   });
 });
