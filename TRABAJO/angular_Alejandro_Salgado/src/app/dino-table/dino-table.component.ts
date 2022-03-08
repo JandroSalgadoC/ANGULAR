820443d50db55986484eaf5bc2c69078578b0c1e
JandroSalgadoC <jandroscjg@gmail.com>
2022-03-08 15:30:40 +0100

    tabla

--- TRABAJO/angular_Alejandro_Salgado/src/app/dino-table/dino-table.component.ts
+++ TRABAJO/angular_Alejandro_Salgado/src/app/dino-table/dino-table.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import Dinosaurios from '../../assets/data/dinosaurs.json';
+import DinosauriosJSON from '../../assets/data/dinosaurs.json';
+interface Dinosaurio{
+        name: String,
+        era: String,
+        diet:String,
+        feetTall: Number,
+        feetLong: Number,
+        pounds: Number,
+        image: String
+}
 
 @Component({
   selector: 'app-dino-table',
@@ -7,6 +16,9 @@ import Dinosaurios from '../../assets/data/dinosaurs.json';
   styleUrls: ['./dino-table.component.css']
 })
 export class DinoTableComponent implements OnInit {
+
+  Dinosuarios: Dinosaurio[] = DinosauriosJSON;
+  imageRoute: string = "../../../../assets/images/"
 
   constructor() { }
 
